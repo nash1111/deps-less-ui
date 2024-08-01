8b59e1e10dfbe5552f069f709efc783974f1a62a
nash1111 <nash1111@users.noreply.github.com>
2024-08-01 18:30:01 +0900

    fix: publish_dir

---
+++ storybook-static/assets/Header.stories-Cqhr-gq5.js
@@ -0,0 +1,7 @@
+import{f as o}from"./Button-Dlp1m998.js";import{H as p}from"./Header-BeV0Ctx5.js";import"./index-DZLKizrv.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const f={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,m,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,L as __namedExportsOrder,f as default};
