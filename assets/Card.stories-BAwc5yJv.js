d68bd349a2e22126893122be6cc89134ba517322
nash1111 <nash1111@users.noreply.github.com>
2024-08-01 09:52:32 +0000

    deploy: ffc6c58a158386466e1f7a68a59052e4ce9de5ca

---
+++ assets/Card.stories-BAwc5yJv.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const i=({title:r,content:d,imageUrl:s})=>e.jsxs("div",{className:"card",children:[s&&e.jsx("img",{src:s,alt:r,className:"card-image"}),e.jsxs("div",{className:"card-body",children:[e.jsx("h2",{className:"card-title",children:r}),e.jsx("p",{className:"card-content",children:d})]})]});i.__docgenInfo={description:"",methods:[],displayName:"Card",props:{title:{required:!0,tsType:{name:"string"},description:""},content:{required:!0,tsType:{name:"string"},description:""},imageUrl:{required:!1,tsType:{name:"string"},description:""}}};const l={title:"Example/Card",component:i,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},t={args:{title:"Card Title",content:"This is the card content"}};var a,n,c;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    title: 'Card Title',
+    content: 'This is the card content'
+  }
+}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const p=["Default"];export{t as Default,p as __namedExportsOrder,l as default};
